134b9b3dcc122069419b403ea0b150d89dac1a1b
anonymous0374 <377738226@qq.com>
2017-10-18 17:03:30 +0800

    add disable status

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,12 +1,31 @@
 <template>
-  <button class='b2b-button'>normal button</button>  
+  <button class='b2b-button' v-bind:class='{ enabled: enabled, disabled: !enabled }' v-on:click='toggleButtonStatus'>normal button</button>  
 </template>
 
 <script>
   export default {
     name: 'b2b-button',
-    data () {
-      return {}
+    enabled: true,
+    data: function () {
+      return {
+        enabled: true
+      }
+    },
+    methods: {
+      disable: function () {
+        this.enabled = false
+      },
+      enable: function () {
+        this.enable = true
+      },
+      toggleButtonStatus: function () {
+        if (!this.enabled) {
+          return false
+        } else {
+          this.enabled = false
+          return false
+        }
+      }
     }
   }
 </script>
@@ -24,9 +43,19 @@
   height: 30px;
 }
 
-.b2b-button:hover {background-color: #5D7AFF}
+.b2b-button.disabled {
+  color: #FFFFFF;
+  background-color: #CCCCCC;
+}
 
-.b2b-button:active {
+.b2b-button.enabled {
+  color: #FFFFFF;
+  background-color: #4565F7;  
+}
+
+.b2b-button.enabled:hover {background-color: #5D7AFF}
+
+.b2b-button.enabled:active {
   background-color: #5D7AFF;
   transform: translateY(2px);
 }
--- src/components/SecondaryButtonComponent.vue
+++ src/components/SecondaryButtonComponent.vue
@@ -1,12 +1,31 @@
 <template>
-  <button class='b2b-secondary-button'>normal button</button>  
+  <button class='b2b-secondary-button' v-bind:class='{ enabled: enabled, disabled: !enabled }' v-on:click='toggleButtonStatus'>secondary</button>  
 </template>
 
 <script>
   export default {
     name: 'b2b-secondary-button',
-    data () {
-      return {}
+    enabled: true,
+    data: function () {
+      return {
+        enabled: true
+      }
+    },
+    methods: {
+      disable: function () {
+        this.enabled = false
+      },
+      enable: function () {
+        this.enable = true
+      },
+      toggleButtonStatus: function () {
+        if (!this.enabled) {
+          return false
+        } else {
+          this.enabled = false
+          return false
+        }
+      }
     }
   }
 </script>
@@ -24,13 +43,25 @@
   height: 30px;
 }
 
-.b2b-secondary-button:hover {
+.b2b-secondary-button.enabled {
+  font-size: 12px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: #4565F7;
+  background-color: #FFFFFF;
+  border: solid 1px #82A7FF;
+  width: 90px;
+  height: 30px;
+}
+
+.b2b-secondary-button.enabled:hover {
   background-color: #5D7AFF;
   box-shadow: 0 0 4px 0 #82A7FF;
   color: #FFFFFF; 
 }
 
-.b2b-secondary-button:active {
+.b2b-secondary-button.enabled:active {
   background-color: #5D7AFF;
   box-shadow: 0 0 4px 0 #82A7FF;
   transform: translateY(2px);
